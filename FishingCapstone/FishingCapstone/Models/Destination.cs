2497f8868a023f565f0ca1f1e8f4d09ed116dd84
mattjohannsen <majohann@hotmail.com>
2020-04-18 20:42:21 -0500

    Added Display Name to Model Properties that are displayed to user

--- FishingCapstone/FishingCapstone/Models/Destination.cs
+++ FishingCapstone/FishingCapstone/Models/Destination.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,8 +10,11 @@ namespace FishingCapstone.Models
     public class Destination
     {
         public int DestinationId { get; set; }
+        [Display(Name = "Destination")]
         public string DestinationName { get; set; }
+        [Display(Name = "Lattitude")]
         public string DestinationLat { get; set; }
+        [Display(Name = "Longitude")]
         public string DestinationLong { get; set; }
         [NotMapped]
         public List<Species> AvailableSpecies { get; set; }
--- FishingCapstone/FishingCapstone/Models/Month.cs
+++ FishingCapstone/FishingCapstone/Models/Month.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace FishingCapstone.Models
     public class Month
     {
         public int MonthId { get; set; }
+        [Display(Name = "Month")]
         public string MonthName { get; set; }
         [NotMapped]
         public CalendarByMonth CalendarByMonth { get; set; }
