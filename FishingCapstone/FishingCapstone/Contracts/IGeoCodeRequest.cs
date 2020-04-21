4cb2aae4329f5d80c54fcddc53632351671a77cf
mattjohannsen <majohann@hotmail.com>
2020-04-20 19:58:21 -0500

    Added GeoLocation.cs, IGeoCodeRequest.cs, and GeoLocationRequest, class, Contract, and Service

---
+++ FishingCapstone/FishingCapstone/Contracts/IGeoCodeRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FishingCapstone.Models;
+
+namespace FishingCapstone.Contracts
+{
+    public interface IGeoCodeRequest
+    {
+        Task<GeoLocation> GetGeoLocation(string address);
+    }
+}
---
+++ FishingCapstone/FishingCapstone/Services/GeoLocationRequest.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FishingCapstone.Contracts;
+using FishingCapstone.Models;
+
+namespace WeekendWindow.Services
+{
+    public class GeoLocationRequest : IGeoCodeRequest
+    {
+        public  GeoLocationRequest()
+        {
+            
+                
+        }
+        public async Task<GeoLocation> GetGeoLocation(string address)
+        {
+            string url = $"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={APIKEYS.GoogleAPIKey}";
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                string json = await response.Content.ReadAsStringAsync();
+                GeoLocation geoLocation = JsonConvert.DeserializeObject<GeoLocation>(json);
+                return geoLocation;
+            }
+            return null;
+        }
+    }
+}
