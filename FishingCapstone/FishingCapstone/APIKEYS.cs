6a0b21ff2d99239a8eb0f614bf8aed3e33a10a10
mattjohannsen <majohann@hotmail.com>
2020-04-20 19:40:52 -0500

    Added APIKEYS.cs

---
+++ FishingCapstone/FishingCapstone/APIKEYS.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeekendWindow
+{
+    public static class APIKEYS
+    {
+        public static readonly string GoogleAPIKey = "YOURAPIKEY";
+
+    }
+}
